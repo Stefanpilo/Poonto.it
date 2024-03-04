149bd7a9be4d86cbc105875d701c49efeb9b980a
Stefanpilo <114323233+Stefanpilo@users.noreply.github.com>
2024-03-04 17:36:28 +0100

    changed burger menu anchors to span

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logo from 'images/PoontoLogo.svg';
 import burgerMenuClosed from 'images/burgerMenuClosed.svg';
@@ -9,7 +9,7 @@ export default function Header() {
     return (
         <div id="header" className="flex boxed">
             <div>
-              <img id="logo" src={logo} onClick={() => window.location = "/"}></img>
+              <img id="logo" src={logo} onClick={() => window.location = "/"} alt='logo'></img>
             </div>
             <div className="flex">
                 <a id="diventa-button" href="/Contatti">Contatti</a>
@@ -32,15 +32,15 @@ function BurgerMenu() {
 
     return (
         <>
-            <img id="burger-menu-icon" src={!isOpened ? burgerMenuClosed : burgerMenuOpened} onClick={clickHandler}/>
+            <img id="burger-menu-icon" src={!isOpened ? burgerMenuClosed : burgerMenuOpened} onClick={clickHandler} alt='burgerMenuIcon'/>
             <div id="menu-dropdown-wrapper" className={isOpened ? "flex opened" : "flex"}>
                 <div id="menu-pages-container" className="flex">
-                    <a id="comunica-menu" className="poontoColor">Comunica.</a>
-                    <a id="amplifica-menu" className="poontoColor">Amplifica.</a>
-                    <a id="diventa-menu" className="poontoColor">Diventa.</a>
-                    <a id="newoos-menu" className="poontoColor">Newoos</a>
+                    <span id="comunica-menu" className="poontoColor">Comunica.</span>
+                    <span id="amplifica-menu" className="poontoColor">Amplifica.</span>
+                    <span id="diventa-menu" className="poontoColor">Diventa.</span>
+                    <span id="newoos-menu" className="poontoColor">Newoos</span>
                 </div>
-                <a id="privacy-policy">Privacy Policy</a>
+                <span id="privacy-policy">Privacy Policy</span>
             </div>
         </>
     );
